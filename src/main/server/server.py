b6c85a9d48d86ebca414ddd24fbe84b7c7d8936f
Leonardo Lima <se.leonardolima@gmail.com>
2023-12-31 16:29:32 -0300

    Add cors middleware

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,11 +1,27 @@
 # pylint: disable=unused-argument
 from fastapi import Depends, FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 
 from src.presentation.dependencies.api_gateway_router import api_gateway_router
 from src.presentation.routers import gateway_routers
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    # allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    # allow_methods=["POST", "GET", "OPTIONS"],
+    # allow_headers=["Content-Type", "Authorization"],
+)
 
 app.include_router(gateway_routers.router, dependencies=[Depends(api_gateway_router)])
 
